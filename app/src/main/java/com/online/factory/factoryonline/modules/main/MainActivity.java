e85b288fa93bf328282611ca033a1252f80a0286
cwenhui <735506583@qq.com>
2016-09-29 15:58:13 +0800

    test for git

--- app/src/main/java/com/online/factory/factoryonline/modules/main/MainActivity.java
+++ app/src/main/java/com/online/factory/factoryonline/modules/main/MainActivity.java
@@ -2,13 +2,11 @@ package com.online.factory.factoryonline.modules.main;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.online.factory.factoryonline.R;
 import com.online.factory.factoryonline.base.BaseActivity;
 import com.online.factory.factoryonline.base.BasePresenter;
-import com.online.factory.factoryonline.modules.splash.SplashActivity;
 
 public class MainActivity extends BaseActivity {
 
