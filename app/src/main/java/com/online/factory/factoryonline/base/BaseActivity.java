dbb8a18d5d6ce6b55138bd737de30726ae5c2bb8
louiszgm <louiszgm@gmail.com>
2016-09-29 16:36:26 +0800

    Fragmentation

--- app/src/main/java/com/online/factory/factoryonline/base/BaseActivity.java
+++ app/src/main/java/com/online/factory/factoryonline/base/BaseActivity.java
@@ -1,24 +1,38 @@
 package com.online.factory.factoryonline.base;
 
 import android.os.Bundle;
+import android.support.annotation.CallSuper;
+import android.support.annotation.CheckResult;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.online.factory.factoryonline.dagger.components.ActivityComponent;
 import com.online.factory.factoryonline.dagger.components.ApplicationComponent;
 import com.online.factory.factoryonline.dagger.modules.ActivityModule;
+import com.trello.rxlifecycle.LifecycleProvider;
+import com.trello.rxlifecycle.LifecycleTransformer;
+import com.trello.rxlifecycle.RxLifecycle;
+import com.trello.rxlifecycle.android.ActivityEvent;
+import com.trello.rxlifecycle.android.RxLifecycleAndroid;
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
+
+import me.yokeyword.fragmentation.SupportActivity;
+import rx.Observable;
+import rx.subjects.BehaviorSubject;
 
 /**
  * Created by louiszgm on 2016/9/29.
  */
 
-public abstract class BaseActivity<V , T extends BasePresenter<V>> extends RxAppCompatActivity {
+public abstract class BaseActivity<V , T extends BasePresenter<V>> extends SupportActivity implements LifecycleProvider<ActivityEvent> {
+    private final BehaviorSubject<ActivityEvent> lifecycleSubject = BehaviorSubject.create();
     private ActivityComponent mComponent;
     protected T mPresenter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        lifecycleSubject.onNext(ActivityEvent.CREATE);
         mPresenter = createPresent();
         if(mPresenter != null){
             V view = (V) this;
@@ -43,11 +57,62 @@ public abstract class BaseActivity<V , T extends BasePresenter<V>> extends RxApp
     }
 
     @Override
+    @NonNull
+    @CheckResult
+    public final Observable<ActivityEvent> lifecycle() {
+        return lifecycleSubject.asObservable();
+    }
+
+    @Override
+    @NonNull
+    @CheckResult
+    public final <T> LifecycleTransformer<T> bindUntilEvent(@NonNull ActivityEvent event) {
+        return RxLifecycle.bindUntilEvent(lifecycleSubject, event);
+    }
+
+    @Override
+    @NonNull
+    @CheckResult
+    public final <T> LifecycleTransformer<T> bindToLifecycle() {
+        return RxLifecycleAndroid.bindActivity(lifecycleSubject);
+    }
+
+
+    @Override
+    @CallSuper
+    protected void onStart() {
+        super.onStart();
+        lifecycleSubject.onNext(ActivityEvent.START);
+    }
+
+    @Override
+    @CallSuper
+    protected void onResume() {
+        super.onResume();
+        lifecycleSubject.onNext(ActivityEvent.RESUME);
+    }
+
+    @Override
+    @CallSuper
+    protected void onPause() {
+        lifecycleSubject.onNext(ActivityEvent.PAUSE);
+        super.onPause();
+    }
+
+    @Override
+    @CallSuper
+    protected void onStop() {
+        lifecycleSubject.onNext(ActivityEvent.STOP);
+        super.onStop();
+    }
+
+    @Override
+    @CallSuper
     protected void onDestroy() {
+        lifecycleSubject.onNext(ActivityEvent.DESTROY);
         super.onDestroy();
         if(mPresenter != null){
             mPresenter.detachView();
         }
-
     }
 }
