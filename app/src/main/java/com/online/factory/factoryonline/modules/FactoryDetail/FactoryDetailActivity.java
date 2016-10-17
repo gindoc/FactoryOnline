359e3515d037014d4c67e2226ad7ecf3a88d8ca2
cwenhui <735506583@qq.com>
2016-10-17 17:50:49 +0800

    factoryDetail界面

---
+++ app/src/main/java/com/online/factory/factoryonline/modules/FactoryDetail/FactoryDetailActivity.java
@@ -0,0 +1,21 @@
+package com.online.factory.factoryonline.modules.FactoryDetail;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.online.factory.factoryonline.R;
+
+/**
+ * Created by cwenhui on 2016/10/17.
+ */
+
+public class FactoryDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_factory_detail);
+    }
+}
